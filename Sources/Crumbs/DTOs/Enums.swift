37bed5567b60f7d4f8e257df8c1c67ba2956cf99
Christopher Wainwright <chris.wainwright@atomicmedia.co.uk>
2025-09-16 18:41:45 +0100

    Make Enums Identifiable
    Add @Available
    
    Expand Restrictions for DTOEnum

--- Sources/Crumbs/DTOs/Enums.swift
+++ Sources/Crumbs/DTOs/Enums.swift
@@ -5,21 +5,29 @@
 //  Created by Christopher Wainwright on 06/09/2025.
 //
 
-
-public protocol DTOEnum: DTO, CaseIterable {}
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public protocol DTOEnum: DTO, Identifiable, CaseIterable {}
 
 public enum Filling: String, DTOEnum {
     case bacon, sausage, egg, vegan_sausage
+    
+    public var id : Self { self }
 }
 
 public enum Bread: String, DTOEnum {
     case white, brown
+    
+    public var id : Self { self }
 }
 
 public enum Sauce: String, DTOEnum {
     case red, brown
+    
+    public var id : Self { self }
 }
 
 public enum CobOrderKind : String, DTOEnum {
     case single, recurring
+    
+    public var id : Self { self }
 }

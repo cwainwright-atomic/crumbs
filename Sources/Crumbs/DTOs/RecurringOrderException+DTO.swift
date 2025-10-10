db10ac42d299583a46ca5c5d0711439377508db1
Christopher Wainwright <chris.wainwright@atomicmedia.co.uk>
2025-10-10 22:12:31 +0100

    Implement Recurring Order Exception DTO

---
+++ Sources/Crumbs/DTOs/RecurringOrderException+DTO.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  crumbs
+//
+//  Created by Christopher Wainwright on 10/10/2025.
+//
+
+import Foundation
+
+public struct RecurringOrderExceptionDTO: DTO {
+    public let id: UUID
+    public let createdAt: Date
+    public let user: UserDTO?
+    public let week: WeekDTO?
+    
+    public init(id: UUID, createdAt: Date, user: UserDTO? = nil, week: WeekDTO? = nil) {
+        self.id = id
+        self.createdAt = createdAt
+        self.user = user
+        self.week = week
+    }
+}

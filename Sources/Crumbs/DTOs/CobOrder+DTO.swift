7a83236dab1ab5e28f55aab5fd041621a6bbfb7f
Christopher Wainwright <chris.wainwright@atomicmedia.co.uk>
2025-10-25 19:46:36 +0100

    Update CobOrderDTO for Compatibility
    - Add WeekListItem and UserListItem DTOs for List APIs
    - Remove optionals from CobOrder

--- Sources/Crumbs/DTOs/CobOrder+DTO.swift
+++ Sources/Crumbs/DTOs/CobOrder+DTO.swift
@@ -12,15 +12,42 @@ public struct CobOrderDTO : Identifiable, DTO {
     public let createdAt: Date
     public let orderDetail: CobOrderDetailDTO
     public let orderKind: CobOrderKind
-    public let user: UserDTO?
-    public let week: WeekDTO?
+    public let user: UserDTO
+    public let week: WeekDTO
     
-    public init(id: UUID, createdAt: Date, orderDetail: CobOrderDetailDTO, orderKind: CobOrderKind, user: UserDTO? = nil, week: WeekDTO? = nil) {
+    public init(id: UUID, createdAt: Date, orderDetail: CobOrderDetailDTO, orderKind: CobOrderKind, user: UserDTO, week: WeekDTO) {
         self.id = id
         self.createdAt = createdAt
         self.orderDetail = orderDetail
         self.orderKind = orderKind
         self.user = user
         self.week = week
+    }
+    
+    public struct WeekListItem: Identifiable, DTO {
+        public let createdAt: Date
+        public let orderDetail: CobOrderDetailDTO
+        public let user: UserDTO
+        public var id: Int { self.hashValue }
+        
+        public init(createdAt: Date, orderDetail: CobOrderDetailDTO, user: UserDTO) {
+            self.createdAt = createdAt
+            self.orderDetail = orderDetail
+            self.user = user
+        }
+        
+    }
+    
+    public struct UserListItem: Identifiable, DTO {
+        public let createdAt: Date
+        public let orderDetail: CobOrderDetailDTO
+        public let week: WeekDTO
+        public var id: Int { self.hashValue }
+        
+        public init(createdAt: Date, orderDetail: CobOrderDetailDTO, week: WeekDTO) {
+            self.createdAt = createdAt
+            self.orderDetail = orderDetail
+            self.week = week
+        }
     }
 }
